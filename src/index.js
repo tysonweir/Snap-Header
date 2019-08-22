95830b12c7f858fc04b2f5f930a0a4709e1ea2dd
Tyson Hughes <thughes@sofi.org>
2019-08-22 10:03:07 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<div class="container">
+  <div class="section1">
+    <div>1</div>
+  </div>
+</div>
+<div class="section2">
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+  <div>7</div>
+  <div>8</div>
+  <div>9</div>
+  <div>10</div>
+  <div>11</div>
+  <div>12</div>
+</div>
+`;
